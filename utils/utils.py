315f4afab58939bdfdaec13e9406405ea263e8c1
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-01-14 23:08:56 +0100

    Mise à jour du notebook en utilisant pie-extended

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,5 @@
+import json
+
 def test():
     print("OK")
 
@@ -19,8 +21,7 @@ def create_json_input_for_collatex(dict_of_text, collate_on="lemmas"):
         witness_dict = {"id": sigla}
         tokens = []
         for token in text:
-            form, case, mood, numb, person, tense, lemma, pos = token
-            morph = case+mood+numb+person+tense
+            form, lemma, pos, morph, *rest = token
             if collate_on == "lemmas":
                 tokens.append({"t": form, "n":lemma, "lemma": lemma, "pos": pos, "morph":morph})
             elif collate_on == "lemmas+pos":
@@ -32,3 +33,30 @@ def create_json_input_for_collatex(dict_of_text, collate_on="lemmas"):
         witness_dict["tokens"] = tokens
         output_dict["witnesses"].append(witness_dict)
     return output_dict
+
+
+def simplify_results(alignment_results):
+    alignment_results_as_json = json.loads(alignment_results)
+    zipped = list(zip(alignment_results_as_json['table'][0], alignment_results_as_json['table'][1], alignment_results_as_json['table'][2]))
+    output_data = list()
+    for locus in zipped:
+        interm_list = []
+        for index, witness in enumerate(locus):
+            if witness is not None:
+                interm_dict = dict()
+                interm_dict['témoin'] = witness[0]['_sigil']
+                interm_dict['forme'] = witness[0]['t']
+                interm_dict['lemme'] = witness[0]['lemma']
+                interm_dict['pos'] = witness[0]['pos']
+                interm_dict['morph'] = witness[0]['morph']
+                interm_list.append(interm_dict)
+            else:
+                interm_dict = dict()
+                interm_dict['témoin'] = alignment_results_as_json["witnesses"][index]
+                interm_dict['forme'] = None
+                interm_dict['lemme'] = None
+                interm_dict['pos'] = None
+                interm_dict['morph'] = None
+                interm_list.append(interm_dict)
+        output_data.append(interm_list)
+    return output_data
